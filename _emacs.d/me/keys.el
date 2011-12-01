5c87f314e7174b9ae972c112e463d92c0f20359b
Fogus <mefogus@gmail.com>
2011-12-01 16:27:11 -0500

    moving old emacs cfg

--- _emacs.d/me/keys.el
+++
@@ -1,3 +0,0 @@
-;; Provide alts to M-x and Esc-x
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
---
+++ old/_emacs.d/init.el
@@ -0,0 +1,32 @@
+(require 'cl)
+
+(defvar emacs-root (if 
+                     (or (eq system-type 'cygwin)
+                         (eq system-type 'gnu/linux)
+                         (eq system-type 'linux))
+                     "/home/fogus/.emacs.d/"
+                     (if (eq system-type 'darwin)
+                       "/Users/fogusm/.emacs.d/"
+                       "c:/home/fogus/.emacs.d/"))
+  "My home directory — the root of my personal emacs load-path.")
+
+(labels ((add-path (p) 
+           (add-to-list 'load-path (concat emacs-root p))))
+  (add-path "me")          ;; all my personal elisp code
+  (add-path "modes")
+  (add-path "modes/scala-mode")
+  (add-path "modes/slime"))
+
+(load-library "settings")  ;; my personal display settings
+(load-library "keys")      ;; key bindings
+(load-library "cfg")       ;; configuration for all of my modes
+(load-library "code")      ;; my code
+
+(require 'yasnippet) 
+(yas/initialize)
+(yas/load-directory (concat emacs-root "modes/snippets"))
+
+(require 'ido)
+
+(server-start)
+(shell "*shell-main*")
