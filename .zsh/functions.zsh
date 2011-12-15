748a17b8fc3ceefd8c1020fd28732b91daea84ab
fogus <mefogus@gmail.com>
2011-12-15 08:31:56 -0500

    Adding reagent function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -107,3 +107,17 @@ function sg {
         script/generate $@
     fi
 }
+
+# Re-acquire forwarded SSH key
+# from http://tychoish.com/rhizome/9-awesome-ssh-tricks/
+function ssh-reagent {
+    for agent in /tmp/ssh-*/agent.*; do
+        export SSH_AUTH_SOCK=$agent
+        if ssh-add -l 2>&1 > /dev/null; then
+            echo Found working SSH Agent:
+            ssh-add -l
+            return
+        fi
+    done
+    echo Cannot find ssh agent - maybe you should reconnect and forward it?
+}
